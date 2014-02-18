7b1bb0980a0c0b536ffaeb97eda44cee005fe4fe
Tom Hulihan <hulihan.tom159@gmail.com>
2014-02-17 23:04:25 -0500

    Add Typist::Error.
    This module will hold all errors intentionally thrown in the gem.

--- lib/typist.rb
+++ lib/typist.rb
@@ -1,4 +1,5 @@
 # This is the top level module for the gem, used as a namespace and as a
 # location for high-level functions.
 module Typist
+  autoload :Error, 'typist/error'
 end
---
+++ lib/typist/error.rb
@@ -0,0 +1,5 @@
+# This module holds all errors thrown in the gem.
+module Typist::Error
+  # This class is never thrown, but can be used as a catch-all.
+  class BaseError < StandardError; end
+end
