b8d83ae5138fada37b484711b8ea04e0193cc4d3
Tom Hulihan <hulihan.tom159@gmail.com>
2014-03-30 10:44:43 -0400

    `autoload` => `require`
    Since `autoload` is not thread-safe, switch it to a `require'.

--- lib/typist.rb
+++ lib/typist.rb
@@ -1,11 +1,11 @@
 # This is the top level module for the gem, used as a namespace and as a
 # location for high-level functions.
 module Typist
-  autoload :Constructor, 'typist/constructor'
-  autoload :Data, 'typist/data'
-  autoload :Error, 'typist/error'
-  autoload :Func, 'typist/func'
-  autoload :Util, 'typist/util'
+  require 'typist/constructor'
+  require 'typist/data'
+  require 'typist/error'
+  require 'typist/func'
+  require 'typist/util'
 
   # Define a new data type.
   def data(*args, &block)
